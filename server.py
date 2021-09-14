063d80225f97edadf69dea6bce2136756ba6d041
korkeep <korkeep@naver.com>
2021-09-14 20:01:28 +0900

    feat: flask api server

--- server.py
+++ server.py
@@ -1,18 +1,73 @@
 import csv
-from flask import Flask, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_restx import Api, Resource
+from werkzeug.utils import secure_filename
 
 # Flask 객체에 API 객체 등록
 app = Flask(__name__)
 api = Api(app)
 
-# CSV 파일을 Dictionary로 변경
-mydict = {}
-with open('./bluehouse/TF_submission_2021-09-13 13_40_59.csv', 'r', encoding='utf-8') as input:
-    reader = csv.reader(input)
-    mydict = {rows[0]:rows[1] for rows in reader}
+# count: index를 나타내기 위함
+# number: 청와대 청원 url에 접근하기 위함
+# petition: 카테고리를 나타내기 위함 
+count = -1
+number = -1
+petition = -1
 
-# '/api/{index}' API 가져오기
+# 읽기: CSV 파일을 Dictionary로 변경
+mydict = {}
+with open('./bluehouse/result-test.csv', 'r', encoding='utf-8') as input:
+    reader = csv.reader(input)
+    for rows in reader:
+        mydict = {rows[0]:rows[1]}
+        count += 1
+
+# /upload: HTML 렌더링
+@app.route('/upload')
+def render_html():
+    return render_template('server.html')
+
+# /input: File 업로드
+@app.route('/input', methods = ['GET', 'POST'])
+def upload_csv():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(secure_filename(f.filename))
+        return redirect(url_for('add_result'))
+
+# /add: CSV 파일에 새로운 Input 분석 결과 추가
+@app.route('/add')
+def add_result():
+    global count
+    global petition
+    global number
+    global mydict
+    with open('./bluehouse/result-input.csv', 'r', encoding='utf-8') as input:
+        first_line_flag = False
+        reader = csv.reader(input)
+        for rows in reader:
+            if not first_line_flag:
+                first_line_flag = True
+                continue
+            number = rows[0]
+            petition = rows[1]
+        f = open('./bluehouse/result-test.csv', 'a', newline='')
+        wr = csv.writer(f)
+        wr.writerow([count, petition])
+        mydict[count] = petition
+        count += 1
+    return redirect(url_for('print_result'))
+
+# /output: 분석 결과 출력
+@app.route('/output')
+def print_result():
+    return {
+        "index": "%s" % str(count - 1),
+        "category": "%s" % mydict[count - 1],
+        "url": "https://www1.president.go.kr/petitions/%s" % number
+    }
+
+# /api/{index}: 학습된 API 가져오기
 @api.route('/api/<string:index>')
 class API(Resource):
     def get(self, index):
@@ -21,6 +76,6 @@ class API(Resource):
             "category": "%s" % mydict[index]
         }
 
-# localhost 80 port에서 서버 구동
+# main: localhost 80 port에서 서버 구동
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=80)
